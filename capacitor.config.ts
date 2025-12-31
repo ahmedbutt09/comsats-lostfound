e0c63b7e288aa522f0f0ab892100978532c75760
ahmedbutt09 <ahmedbutt1109@gmail.com>
2025-12-31 09:16:17 +0500

    docs: added assets and configured splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,21 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.findit.app',
   appName: 'COMSATS LOST & FOUND',
-  webDir: 'build'
+  webDir: 'build',
+  // Add the plugins section below
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#003366", // COMSATS Blue
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      spinnerColor: "#FF6600", // COMSATS Orange
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
